a85a8ac38a3d8cb729f9062541fe58abd5151cf4
Shohjahon <mahmudjonovshohjahon96@gmail.com>
2023-03-04 17:12:08 +0500

    v1

---
+++ .js
@@ -0,0 +1,29 @@
+
+
+//console.log('Я ' + myAge +'. мне ' + age + ' лет');
+
+var myName = prompt('введите своё имя');
+var text = prompt('введите cвой возраст');
+
+console.log(' Меня зовут ' + myName);
+console.log(' Мне ' + text +' лет');
+
+var number1 = prompt('решите пример 10 + 120 = ?')
+var number2 = prompt('решите пример 1180 - 58 = ?')
+var number3 = prompt('решите пример 1180 / 20 = ?')
+var number4 = prompt('решите пример 260 % 26 = ?' );
+
+console.log(' Пример 1: 10 + 120 = ' + (10 + 120) + ' Ваш ответ ' +number1);
+console.log(' Пример 2: 1180 + 58 = ' + (1180 + 58) + ' Ваш ответ '+number2);
+console.log(' Пример 3: 1180 / 20 = ' + 1180 / 20 + ' Ваш ответ ' +number3);
+console.log(' Пример 4: 260 % 26 = ' + 260 % 27 + ' Ваш ответ '+ number4);
+
+var X = +prompt("введите первое число")
+console.log();
+var Y = +prompt("введите второе число")
+console.log();
+var Z = +prompt("введите третье число")
+console.log();
+alert('Среднее арифметическое '+(X+Y+Z)/3)
+
+alert('можете посмотреть свои ответы в консоле')
